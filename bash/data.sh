fae9c9117dfb7db40d2c68348abbdc8e6b414809
tokestermw <tokestermw@gmail.com>
2013-11-01 10:20:41 -0700

    import table worked

--- bash/data.sh
+++ bash/data.sh
@@ -5,13 +5,18 @@ cat ../data/measurements.csv | cut -d, -f1-5 > ../data/measurements_cut.csv
 
 ## change tsv into a csv for station_data
 awk '{gsub("\t",",",$0); print;}' station_data.tsv > station_data.csv
-station_data_cut.csv
+cat ../data/station_data.csv | cut -d, -f1-3 > ../data/station_data_cut.csv
 
 ## subset data to only 2011
-grep 2011- station_data.csv > station_data_2011.csv
+grep 2011- station_data_cut.csv > station_data_2011.csv
 grep 2011- measurements_cut.csv > measurements_2011.csv
-
 
 ## subset gov data from Fukushima, Ibaraki, Miyagi,
 
 ## convert station_id to lat lon
+
+# fukushima daiichi
+# 37deg25'22.7'' N 141deg01' 58.5'' 
+# 37.422972,141.032917
+# check radius
+# http://www.movable-type.co.uk/scripts/latlong.html
--- sql/data.sql
+++ sql/data.sql
@@ -1,8 +1,9 @@
 -- join the lat lons of station_data_2011.csv and station_id.csv
-create table station_data (station_id text, datetime text, val float, 
+create table station_data(station_id text, datetime text, val float)
 .separator ","
-.import 
+.import 'station_data_2011.csv' station_data
 
-create table measurements ("captured time" text, latitude float, longitude float, value float, 
+create table station_id(station_id text, site_id text, site_name text, prefecture_name text, lat float, lon float);
+.import 'station_id_cut.csv' station_id
 
 ## convert station_id to lat lon
