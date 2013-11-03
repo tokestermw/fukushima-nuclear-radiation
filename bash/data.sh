f27a17752061858bf48ed7016d8ad9e69ee54c38
tokestermw <tokestermw@gmail.com>
2013-11-02 18:02:03 -0700

    almost done with adding script

--- bash/data.sh
+++ bash/data.sh
@@ -11,9 +11,16 @@ cat ../data/station_data.csv | cut -d, -f1-3 > ../data/station_data_cut.csv
 grep 2011- station_data_cut.csv > station_data_2011.csv
 grep 2011- measurements_cut.csv > measurements_2011.csv
 
+## remove the commas from Horiguchi, Hitachinaka City and Ishikawa, Mito
+awk '{gsub("guchi, Hitachi", "guchi Hitachi", $0); print;}' station_id.csv > station_idtemp.csv && mv station_idtemp.csv station_id.csv
+awk '{gsub("kawa, Mito", "kawa Mito", $0); print;}' station_id.csv > station_idtemp.csv && mv station_idtemp.csv station_id.csv
+
 ## remove japanese columns from station_id.csv
 cat ../data/station_id.csv | cut -d, -f1,2,4,7,12,13 > ../data/station_id_cut.csv
 
-## subset gov data from Fukushima, Ibaraki, Miyagi,
+## now run ../sql/data.sql to join the station_id and station_data
+
+## run ../python/radius.py, to subset wrt radius
 
 ## remove all intermediate files to save harddrive space
+
--- python/radius.py
+++ python/radius.py
@@ -2,7 +2,7 @@
 Grab all the radiation measurements within x km of epicenter.
 
 Location of Fukushima Daiichi
-37deg25'22.7'' N 141deg01' 58.5''
+37ª25'22.7'' N 141ª01' 58.5''
 37.422972,141.032917
 
 Web app to translate distance on Earth
@@ -13,12 +13,13 @@ http://www.platoscave.net/blog/2009/oct/5/calculate-distance-latitude-longitude-
 """
 
 FUKUSHIMA_DAIICHI = (37.422972, 141.032917)
+RADIUS = 6371 # Earth's radius (km)
 
 from math import radians, sin, cos, atan2, sqrt
 
 def scan(startpoint, endpoint, km):
     """
-    Calculate between two lat longs, and check to see if they are with xkm.
+    Calculate distance between two lat longs, and check to see if they are with xkm.
     Uses the Haversine formula:
       http://en.wikipedia.org/wiki/Haversine_formula
 
@@ -28,7 +29,6 @@ def scan(startpoint, endpoint, km):
     """
     latA, lonA = startpoint
     latB, lonB = endpoint
-    radius = 6371 # Earth's radius (km)
 
     dlat = radians(latB - latA)
     dlon = radians(lonB - lonA)
@@ -36,17 +36,30 @@ def scan(startpoint, endpoint, km):
       * cos(math.radians(latB)) * sin(dlon/2) * sin(dlon/2)
     c = 2 * atan2(sqrt(a), sqrt(1-a))
 
-    dist = radius * c
+    dist = RADIUS * c
 
     return km - dist > 0
 
 import os
 
-def append_scan_to_data(fname, column):
+def append_scan_to_data(fname, columns, km):
+    """
+    Adds TRUE FALSE column if lat lon is close enough to fukushima daiichi.
+
+    columns a list of lat, lon columns (e.g. [12,13])
+    """
+    lat, lon = columns
+    isMeasurementClose = []
     with open(fname) as f:
         for line in f:
-            
-    scan()
-    pass
+            splt = line.split(',')
+            endpoint = tuple([float(i) for i in splt[lat:(lon+1)]])
+            isMeasurementClose.append(scan(FUKUSHIMA_DAIICHI, endpoint, km))
+
+    return isMeasurementClose
 
 if __name__ == '__main__':
+    fname, columns, km = [i for i in sys.argv]
+    append_scan_to_data(fname, columns, km)
+
+    
