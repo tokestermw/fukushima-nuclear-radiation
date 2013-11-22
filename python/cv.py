e35187e2498f74b8cb98fce18b7bb184e6d6e9a7
tokestermw <tokestermw@gmail.com>
2013-11-22 08:58:03 -0800

    fix css

--- python/cv.py
+++ python/cv.py
@@ -2,7 +2,7 @@ import numpy as np
 import sqlite3 as sql
 import matplotlib.pyplot as plt
 import seaborn
-from interpolate import interpolate 
+from interpolate import interpolate
 
 ## kriging cross validation
 def cv(lat, lon, val, model_par):
@@ -50,5 +50,40 @@ plt.close()
 ## performance between methods wrt to lat, lon
 model = interpolate(d[:,0], d[:,1], np.log(d[:,2]))
 
-
 ## universal kriging or regression since central mean (so see error at the highest point)
+
+## now get citizen data
+m = c.execute("""
+select lat, lon, val from measurements_thin where datetime = "2011-09-24"
+""").fetchall()
+m = np.array(m)
+
+## kriging interpolate and get the average difference between the two!
+smooth = interpolate(d[:,0], d[:,1], np.log(d[:,2]))
+smooth.pick_points(m[:,0], m[:,1])
+smooth.kriging()
+
+np.median(np.log(m[:,2] / 300.0 * 1000.0) / smooth.z)
+# 0.98836025991936916
+
+np.mean(np.log(m[:,2] / 300.0 * 1000.0) / smooth.z)
+# 0.98464300976754637
+
+def compare_median(x):
+    out = np.zeros(len(x))
+    for i in range(len(x)):
+        out[i] = np.median(np.log(m[:,2] / x[i] * 1000.0) / smooth.z)
+    return out
+
+# now plot median for every conversion
+x = np.array(range(100, 500, 5))
+y = compare_median(x)
+
+# 270 is best
+plt.plot(x, y, 'r', markersize = 10)
+plt.axhline(y = 1.0, xmin = 0, xmax = 1000)
+plt.ylabel("Leave One Out Cross-Validation Score")
+plt.xlabel("Log(Radiation)")
+plt.savefig("plots/conversion_profile.pdf")
+plt.close()
+
